cd27aa98fe26a8e965404c6cc85e87c458465342
lubana-santy <satvindersingh1276@gmail.com>
2019-10-13 07:50:55 +0530

    Linked List 2

---
+++ LinkedList2/ReverseLL(Recursive).java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static LinkedListNode<Integer> reverse(LinkedListNode<Integer> head, LinkedListNode<Integer> headRef)
+    {
+        LinkedListNode<Integer> first;
+        LinkedListNode<Integer> rest;
+        if(head == null)
+            return head;
+        first = head;
+        rest = first.next;
+        if(rest == null)
+        {
+            headRef = first;
+            return headRef;
+        }
+        headRef = reverse(rest , headRef);
+        rest.next = first;
+        first.next = null;
+        return headRef;
+    }
+    
+	public static LinkedListNode<Integer> reverse_R(LinkedListNode<Integer> head) {
+		
+        return reverse(head , head);
+    }
+}
+
+class LinkedListNode<T> {
+	public T data;
+	public LinkedListNode<T> next;
+
+	public LinkedListNode(T data) {
+		this.setData(data);
+		this.next = null;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+}
+public class Runner {
+	
+	private static Scanner s= new Scanner(System.in);
+	public static void main(String[] args) {
+		print(Solution.reverse_R(input()));
+	}
+	public static void print(LinkedListNode<Integer> head) {
+		while (head != null) {
+			System.out.print(head.getData() + " ");
+			head = head.next;
+		}
+	}
+	public static LinkedListNode<Integer> input() {
+		int data = s.nextInt();
+		
+		LinkedListNode<Integer> head = null;
+		LinkedListNode<Integer> tail = null;
+		while (data!=-1) {
+			LinkedListNode<Integer> newNode = new LinkedListNode<Integer>(data);
+			if (head == null) {
+				head = newNode;
+				tail = newNode;
+			} else {
+				tail.next = newNode;
+				tail = newNode;
+			}
+			data = s.nextInt();
+		}
+		return head;
+	}
+}
+
