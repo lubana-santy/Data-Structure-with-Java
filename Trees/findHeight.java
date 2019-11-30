2feee93a25d0b313ec9bb85b0b6a46ced87086f7
lubana-santy <satvindersingh1276@gmail.com>
2019-11-30 11:11:12 +0530

    Trees

---
+++ Trees/findHeight.java
@@ -0,0 +1,145 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution {
+
+	public static int height(TreeNode<Integer> root){
+		QueueUsingLL<TreeNode<Integer>> pending = new QueueUsingLL<TreeNode<Integer>>();
+        int height = 0;
+        pending.enqueue(root);
+        while(!pending.isEmpty())
+        {
+            int size = pending.size();
+            height+=1;
+            TreeNode<Integer> front = null;
+            for(int i = 0; i < size; i++)
+            {
+                try{
+                    front = pending.dequeue();
+                }
+                catch(QueueEmptyException e)
+                {}
+                for(int j = 0; j < front.children.size(); j++)
+                {
+                    pending.enqueue(front.children.get(j));
+                }
+            }
+        }
+        return height;
+	}
+
+}
+
+class QueueEmptyException extends Exception {
+}
+
+class QueueUsingLL<T> {
+
+	class Node<T> {
+		T data;
+		Node<T> next;
+		Node(T data){
+			this.data = data;
+		}
+	}
+
+	private Node<T> head;
+	private Node<T> tail;
+	private int size = 0;
+
+	public int size(){
+		return size;
+	}
+
+	public boolean isEmpty(){
+		if(size == 0){
+			return true;
+		}
+		return false;
+	}
+
+	public T front() throws QueueEmptyException{
+		if(size == 0){
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+
+		return head.data;
+	}
+
+
+	public void enqueue(T element){
+		Node<T> newNode = new Node<T>(element);
+
+		if(head == null){
+			head = newNode;
+			tail = newNode;
+		}
+		else{
+			tail.next = newNode;
+			tail = newNode;
+		}
+
+		size++;
+	}
+
+	public T dequeue() throws QueueEmptyException{
+		if(head == null){
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+		if(head == tail){
+			tail = null;
+		}
+		T temp = head.data;
+		head = head.next;
+		size--;
+		return temp;
+	}
+}
+
+class TreeNode<T> {
+	T data;
+	ArrayList<TreeNode<T>> children;
+
+	TreeNode(T data){
+		this.data = data;
+		children = new ArrayList<TreeNode<T>>();
+	}
+}
+
+public class Main {
+
+
+
+	static Scanner s = new Scanner(System.in);
+
+	public static TreeNode<Integer> takeInputLevelWise(){
+		QueueUsingLL<TreeNode<Integer>> pendingNodes = new QueueUsingLL<TreeNode<Integer>>();  // Queue of node that are entered themselves but their children aren't added yet
+		int rootData = s.nextInt();
+		TreeNode<Integer> root = new TreeNode<Integer>(rootData);
+		pendingNodes.enqueue(root);
+		while(!pendingNodes.isEmpty()){
+			TreeNode<Integer> currentNode;
+			try {
+				currentNode = pendingNodes.dequeue();
+				int numChild = s.nextInt();
+				for(int i = 0 ; i < numChild; i++){
+					int currentChild = s.nextInt();
+					TreeNode<Integer> childNode = new TreeNode<Integer>(currentChild);
+					pendingNodes.enqueue(childNode);
+					currentNode.children.add(childNode);
+				}
+			} catch (QueueEmptyException e) {
+			}
+		}
+		return root;
+	}
+
+	public static void main(String[] args) {
+		TreeNode<Integer> root =  takeInputLevelWise();
+		System.out.println(Solution.height(root));
+
+	}
+
+}
