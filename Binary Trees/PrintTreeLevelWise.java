ab47a99211971aff5821db52e5ebd3de709c174a
lubana-santy <satvindersingh1276@gmail.com>
2019-12-12 10:10:33 +0530

    Binary Trees

---
+++ Binary Trees/PrintTreeLevelWise.java
@@ -0,0 +1,163 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution {
+    
+	public static void printLevelWise(BinaryTreeNode<Integer> root) {
+	    QueueUsingLL<BinaryTreeNode<Integer>> q = new QueueUsingLL<>();
+        q.enqueue(root);
+        while(!q.isEmpty())
+        {
+            BinaryTreeNode<Integer> front = null;
+            try{
+                front = q.dequeue();
+            }
+            catch(QueueEmptyException e)
+            {
+                
+            }
+            System.out.print(front.data + ":");
+            if(front.left != null)
+            {
+                q.enqueue(front.left);
+                System.out.print("L:" + front.left.data + ",");
+            }
+            else
+            {
+                System.out.print("L:-1,");
+            }
+            if(front.right != null)
+            {
+                q.enqueue(front.right);
+                System.out.println("R:" + front.right.data );
+            }
+            else
+            {
+                System.out.println("R:-1");
+            }
+            
+            
+        }
+		
+	}
+	
+}
+
+
+class QueueEmptyException extends Exception {
+}
+
+class QueueUsingLL<T> {
+
+	class Node<T> {
+		T data;
+		Node<T> next;
+		Node(T data){
+			this.data = data;
+		}
+	}
+
+	private Node<T> head;
+	private Node<T> tail;
+	private int size = 0;
+
+	public int size(){
+		return size;
+	}
+
+	public boolean isEmpty(){
+		if(size == 0){
+			return true;
+		}
+		return false;
+	}
+
+	public T front() throws QueueEmptyException{
+		if(size == 0){
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+
+		return head.data;
+	}
+
+
+	public void enqueue(T element){
+		Node<T> newNode = new Node<T>(element);
+
+		if(head == null){
+			head = newNode;
+			tail = newNode;
+		}
+		else{
+			tail.next = newNode;
+			tail = newNode;
+		}
+
+		size++;
+	}
+
+	public T dequeue() throws QueueEmptyException{
+		if(head == null){
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+		if(head == tail){
+			tail = null;
+		}
+		T temp = head.data;
+		head = head.next;
+		size--;
+		return temp;
+	}
+}
+
+class BinaryTreeNode<T> {
+	T data;
+	BinaryTreeNode<T> left;
+	BinaryTreeNode<T> right;
+
+	public BinaryTreeNode(T data) {
+		this.data = data;
+	}
+}
+
+public class Runner {
+	static Scanner s = new Scanner(System.in);
+
+	public static BinaryTreeNode<Integer> takeInput(){
+		QueueUsingLL<BinaryTreeNode<Integer>>  pendingNodes = new QueueUsingLL<BinaryTreeNode<Integer>>(); // we can skip writing again inside <> after java version 1.7 
+		Scanner s = new Scanner(System.in);
+		int rootData = s.nextInt();
+		BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootData);
+		pendingNodes.enqueue(root);
+
+		while(!pendingNodes.isEmpty()){
+			BinaryTreeNode<Integer> currentNode;
+			try {
+				currentNode = pendingNodes.dequeue();
+			} catch (QueueEmptyException e) {
+				return null;
+			}
+			int leftChildData = s.nextInt();
+			if(leftChildData != -1){
+				BinaryTreeNode<Integer> leftChild = new BinaryTreeNode<Integer>(leftChildData);
+				currentNode.left = leftChild;
+				pendingNodes.enqueue(leftChild);
+			}
+			int rightChildData = s.nextInt();
+			if(rightChildData != -1){
+				BinaryTreeNode<Integer> rightChild = new BinaryTreeNode<Integer>(rightChildData);
+				currentNode.right = rightChild;
+				pendingNodes.enqueue(rightChild);
+			}
+		}
+		return root;
+	}
+
+
+	public static void main(String[] args) {
+		BinaryTreeNode<Integer> root = takeInput();
+		Solution.printLevelWise(root);
+	}
+}
