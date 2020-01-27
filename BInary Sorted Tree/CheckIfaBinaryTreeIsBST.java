e7ca51a1e68221e3ee954ebdd1643259af3415b3
SATVINDER SINGH <satvindersingh1276@gmail.com>
2020-01-27 18:50:39 +0530

    Binary Sorted Trees

---
+++ BInary Sorted Tree/CheckIfaBinaryTreeIsBST.java
@@ -0,0 +1,180 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class QueueEmptyException extends Exception {
+}
+
+class QueueUsingLL<T> {
+
+	class Node<T> {
+		T data;
+		Node<T> next;
+		Node(T data){
+			this.data = data;
+		}
+	}
+
+	private Node<T> head;
+	private Node<T> tail;
+	private int size = 0;
+
+	public int size(){
+		return size;
+	}
+
+	public boolean isEmpty(){
+		if(size == 0){
+			return true;
+		}
+		return false;
+	}
+
+	public T front() throws QueueEmptyException{
+		if(size == 0){
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+
+		return head.data;
+	}
+
+
+	public void enqueue(T element){
+		Node<T> newNode = new Node<T>(element);
+
+		if(head == null){
+			head = newNode;
+			tail = newNode;
+		}
+		else{
+			tail.next = newNode;
+			tail = newNode;
+		}
+
+		size++;
+	}
+
+	public T dequeue() throws QueueEmptyException{
+		if(head == null){
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+		if(head == tail){
+			tail = null;
+		}
+		T temp = head.data;
+		head = head.next;
+		size--;
+		return temp;
+	}
+}
+
+class BinaryTreeNode<T> {
+	T data;
+	BinaryTreeNode<T> left;
+	BinaryTreeNode<T> right;
+
+	public BinaryTreeNode(T data) {
+		this.data = data;
+	}
+}
+
+public class Solution {
+
+    public static int maximum(BinaryTreeNode<Integer> root){
+        if(root == null)
+            return Integer.MIN_VALUE;
+        return Math.max(root.data,Math.max(maximum(root.left),maximum(root.right)));    
+    }
+    public static int minimum(BinaryTreeNode<Integer> root){
+        if(root == null)
+            return Integer.MAX_VALUE;
+        return Math.min(root.data,Math.min(minimum(root.left),minimum(root.right)));
+    }
+	public static boolean isBST(BinaryTreeNode<Integer> root) {
+		if(root == null)
+            return true;
+        int leftMax = maximum(root.left);
+        int rightMin = minimum(root.right);
+        if(root.data <= leftMax)
+            return false;
+        if(root.data > rightMin)
+            return false;
+        boolean isLeftBST = isBST(root.left);
+        boolean isRightBST = isBST(root.right);
+        if(isLeftBST && isRightBST)
+            return true;
+        else
+            return false;
+	}
+}
+
+public class Main {
+	static Scanner s = new Scanner(System.in);
+
+	public static BinaryTreeNode<Integer> takeInput(){
+		QueueUsingLL<BinaryTreeNode<Integer>>  pendingNodes = new QueueUsingLL<BinaryTreeNode<Integer>>(); // we can skip writing again inside <> after java version 1.7 
+		Scanner s = new Scanner(System.in);
+		int rootData = s.nextInt();
+		BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootData);
+		pendingNodes.enqueue(root);
+
+		while(!pendingNodes.isEmpty()){
+			BinaryTreeNode<Integer> currentNode;
+			try {
+				currentNode = pendingNodes.dequeue();
+			} catch (QueueEmptyException e) {
+				return null;
+			}
+			int leftChildData = s.nextInt();
+			if(leftChildData != -1){
+				BinaryTreeNode<Integer> leftChild = new BinaryTreeNode<Integer>(leftChildData);
+				currentNode.left = leftChild;
+				pendingNodes.enqueue(leftChild);
+			}
+			int rightChildData = s.nextInt();
+			if(rightChildData != -1){
+				BinaryTreeNode<Integer> rightChild = new BinaryTreeNode<Integer>(rightChildData);
+				currentNode.right = rightChild;
+				pendingNodes.enqueue(rightChild);
+			}
+		}
+		return root;
+	}
+
+	public static void printLevelWiseAtDiffLevel(BinaryTreeNode<Integer> root){
+		QueueUsingLL<BinaryTreeNode<Integer>>  primary = new QueueUsingLL<BinaryTreeNode<Integer>>();
+		QueueUsingLL<BinaryTreeNode<Integer>>  secondary = new QueueUsingLL<BinaryTreeNode<Integer>>();
+
+		primary.enqueue(root);
+
+		while(!primary.isEmpty()){
+			BinaryTreeNode<Integer> current=null;
+			try {
+				current = primary.dequeue();
+			} catch (QueueEmptyException e) {
+				System.out.println("Not possible");
+			}
+			System.out.print(current.data + " ");
+			if(current.left != null){
+				secondary.enqueue(current.left);
+			}
+			if(current.right != null){
+				secondary.enqueue(current.right);
+			}
+			if(primary.isEmpty()){
+				QueueUsingLL<BinaryTreeNode<Integer>>  temp = secondary;
+				secondary = primary;
+				primary = temp;
+				System.out.println();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		BinaryTreeNode<Integer> root = takeInput();
+		System.out.println(Solution.isBST(root));
+	}
+
+}
